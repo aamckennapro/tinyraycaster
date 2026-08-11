dffb78adfada1b36cb01fe67454913680030a53a
aamckennapro <aamckennapro@gmail.com>
2026-08-11 16:47:56 -0400

    Part 3 begins, lots of heavy math

--- tinyraycaster.cc
+++ tinyraycaster.cc
@@ -10,22 +10,51 @@
 #include "map.h"
 #include "utils.h"
 #include "player.h"
+#include "sprite.h"
 #include "framebuffer.h"
 #include "textures.h"
 
-int wall_x_texcoord(const float x, const float y, Texture &tex_walls) {
-  float hitx = x - floor(x+.5);
-  float hity = y - floor(y+.5);
-  int tex = hitx*tex_walls.size;
-  if (std::abs(hity)>std::abs(hitx))
-    tex = hity*tex_walls.size;
+int wall_x_texcoord(const float hitx, const float hity, Texture &tex_walls) {
+  float x = hitx - floor(hitx+.5);
+  float y = hity - floor(hity+.5);
+  int tex = x*tex_walls.size;
+  if (std::abs(y)>std::abs(x))
+    tex = y*tex_walls.size;
   if (tex<0)
     tex += tex_walls.size;
   assert(tex>=0 && tex<(int)tex_walls.size);
   return tex;
 }
 
-void render(FrameBuffer &fb, Map &map, Player &player, Texture &tex_walls) {
+void map_show_sprite(Sprite &sprite, FrameBuffer &fb, Map &map) {
+  const size_t rect_w = fb.w/(map.w*2);
+  const size_t rect_h = fb.h/map.h;
+  fb.draw_rectangle(sprite.x*rect_w-3, sprite.y*rect_h-3, 6, 6, pack_color(255,0,0));
+}
+
+void draw_sprite(Sprite &sprite, FrameBuffer &fb, Player &player, Texture &tex_sprites) {
+  // absolute direction from player to sprite - radians
+  float sprite_dir = atan2(sprite.y - player.y, sprite.x - player.x);
+  while (sprite_dir - player.a > M_PI) sprite_dir -= 2*M_PI;
+
+  while (sprite_dir - player.a < -M_PI) sprite_dir += 2*M_PI;
+
+  float sprite_dist = std::sqrt(pow(player.x - sprite.x, 2) + pow(player.y - sprite.y, 2));
+  size_t sprite_screen_size = std::min(1000, static_cast<int>(fb.h/sprite_dist));
+  int h_offset = (sprite_dir - player.a)/player.fov*(fb.w/2) + (fb.w/2)/2 - tex_sprites.size/2;
+
+  int v_offset = fb.h/2 - sprite_screen_size/2;
+
+  for (size_t i=0; i<sprite_screen_size; i++) {
+    if (h_offset+i<0 || h_offset+i>=fb.w/2) continue;
+    for (size_t j=0; j<sprite_screen_size; j++) {
+      if (v_offset+j<0 || v_offset+j>=fb.h) continue;
+      fb.set_pixel(fb.w/2 + h_offset+i, v_offset+j, pack_color(0,0,0));
+    }
+  }
+}
+
+void render(FrameBuffer &fb, Map &map, Player &player, std::vector<Sprite> &sprites, Texture &tex_walls, Texture &tex_monst) {
   fb.clear(pack_color(255, 255, 255));
   const size_t rect_w = fb.w/(map.w*2);
   const size_t rect_h = fb.h/map.h;
@@ -58,7 +87,8 @@ void render(FrameBuffer &fb, Map &map, Player &player, Texture &tex_walls) {
 
       size_t texid = map.get(x, y);
       assert(texid<tex_walls.count);
-      size_t column_height = fb.h/(t*cos(angle-player.a));
+      float dist = t*cos(angle-player.a);
+      size_t column_height = fb.h/dist;
       int x_texcoord = wall_x_texcoord(x, y, tex_walls);
       std::vector<uint32_t> column = tex_walls.get_scaled_column(texid, x_texcoord, column_height); // column textures
       int pix_x = i + fb.w/2;
@@ -67,11 +97,15 @@ void render(FrameBuffer &fb, Map &map, Player &player, Texture &tex_walls) {
         if (pix_y>=0 && pix_y<(int)fb.h) {
           fb.set_pixel(pix_x, pix_y, column[j]); // This handles drawing in
                                                  // 3D.
-          fb.set_pixel(96 + fb.w/2, 130 + fb.h/2 - column_height/2, pack_color(0, 0, 0));
         }
       }
       break;
     }
+  }
+
+  for (size_t i=0; i<sprites.size(); i++) {
+    map_show_sprite(sprites[i], fb, map);
+    draw_sprite(sprites[i], fb, player, tex_monst);
   }
 }
 
@@ -81,12 +115,14 @@ int main() {
   Player player{3., 3, 1.523, M_PI/3};
   Map map;
   Texture tex_walls("./walltext.png");
-  if (!tex_walls.count) {
-    std::cerr << "Failed to load wall textures" << std::endl;
+  Texture tex_monst("./monsters.png");
+  if (!tex_walls.count || !tex_monst.count) {
+    std::cerr << "Failed to load textures" << std::endl;
     return -1;
   }
+  std::vector<Sprite> sprites{ {1.834, 8.75, 0}, {5.323, 5.365, 1}, {4.123, 10.265, 1} };
 
-  for (size_t frame=0; frame<360; frame++) {
+/*  for (size_t frame=0; frame<360; frame++) {
     std::stringstream ss;
     ss << std::setfill('0') << std::setw(5) << frame << ".ppm";
     player.a += 2*M_PI/360;
@@ -94,6 +130,8 @@ int main() {
     render(fb, map, player, tex_walls);
     drop_ppm_image(ss.str(), fb.img, fb.w, fb.h);
   }
-
+*/
+  render(fb, map, player, sprites, tex_walls, tex_monst);
+  drop_ppm_image("./out.ppm", fb.img, fb.w, fb.h);
   return 0;
 }
